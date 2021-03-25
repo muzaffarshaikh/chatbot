330c1dbc1d08b36e502ae730d5be0c8796b760ac
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-03-25 14:11:54 +0530

    Minor testing changes

--- backend/vectorizer.py
+++ backend/vectorizer.py
@@ -1,20 +1,23 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tokenizer import sent_tokens
 
-print(sent_tokens.__len__())
+# print(sent_tokens.__len__())
 
 vectorizer = TfidfVectorizer();
 tfidf = vectorizer.fit_transform(sent_tokens)
 
-print(vectorizer.get_feature_names())
-print(vectorizer.get_feature_names().__len__())
+# print(vectorizer.get_feature_names())
+# print(vectorizer.get_feature_names().__len__())
 
-print(tfidf.toarray())
+# print(tfidf.toarray())
 
 arr = tfidf.toarray()
 
-for entry in arr:
-    print(entry)
-
+# for entry in arr:
+#     print(entry)
+#
 print("\nThe shape of the TF-IDF matrix is: ", tfidf.shape)
+print(tfidf)
+print(arr)
+
 

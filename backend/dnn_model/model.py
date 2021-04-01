707482a31b7ec6542326baf5dab3d555bfa7f673
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-04-01 13:03:56 +0530

    tweaks with activation function and optimizers

--- backend/dnn_model/model.py
+++ backend/dnn_model/model.py
@@ -9,7 +9,7 @@ def create_model(train_features, train_classes):
     net = tflearn.fully_connected(net, 10)
     net = tflearn.fully_connected(net, 10)
     net = tflearn.fully_connected(net, len(train_classes[0]), activation='softmax')
-    net = tflearn.regression(net, optimizer='adam')
+    net = tflearn.regression(net, optimizer='SGD', learning_rate=0.01)
     model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
     model.fit(train_features, train_classes, n_epoch=1000, batch_size=8, show_metric=True)
     return model

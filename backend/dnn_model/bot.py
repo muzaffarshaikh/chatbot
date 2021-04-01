a1199cf8597ea4e201637515084204c096a83cd6
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-04-01 20:20:53 +0530

    Matched the dimensions of feature and class vectors.

--- backend/dnn_model/bot.py
+++ backend/dnn_model/bot.py
@@ -6,7 +6,8 @@ import numpy as np
 from memory_growth import memory_growth_exception
 from preprocess import punctuation_removal
 from model import create_model
-from input_data import train_features, train_classes, test_features, features
+from input_data import train_features, train_classes, test_features, features, words
+from user_input import input_sentence_words, bag_of_words_user_query
 
 stemmer = LancasterStemmer()
 
@@ -17,10 +18,16 @@ model = create_model(features, train_classes)
 # model.save('model.tflearn')
 # model.load('./model.tflearn')
 
+temp_train_classes = []
+
+for t in train_classes:
+    temp_train_vector = np.pad(t, (0, 52), 'constant')
+    temp_train_classes.append(temp_train_vector)
+
 
 # Evaluate model
-# score = model.evaluate(test_features, train_classes)
-# print(score)
+score = model.evaluate(test_features, temp_train_classes)
+print(score)
 #
 # predictions = model.predict(test_features)
 # predictions = np.array([np.argmax(pred) for pred in predictions])
@@ -31,14 +38,11 @@ model = create_model(features, train_classes)
 # print(acc)
 
 # Run the model on one example
-# prediction = model.predict()
-# print(prediction)
+prediction = model.predict()
+print(prediction)
 
+# user_sentence = input_sentence_words(input())
 
-def input_sentence(sentence):
-    # tokenizing the pattern
-    sentence = punctuation_removal(sentence)
-    sentence_words = nltk.word_tokenize(sentence)
-    # stemming each word
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
+# bog_array = bag_of_words_user_query(user_sentence, words)
+
+# print(bog_array)

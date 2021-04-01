34db0ac0043aedf1f2d1abe0a2a561c907246647
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-04-01 13:05:44 +0530

    Score and Predictions

--- backend/dnn_model/bot.py
+++ backend/dnn_model/bot.py
@@ -1,29 +1,38 @@
 import nltk
 from nltk import LancasterStemmer
 
+import numpy as np
+
 from memory_growth import memory_growth_exception
 from preprocess import punctuation_removal
 from model import create_model
-from input_data import train_features, train_classes
-
+from input_data import train_features, train_classes, test_features, features
 
 stemmer = LancasterStemmer()
 
 memory_growth_exception()
 
-model = create_model(train_features, train_classes)
+model = create_model(features, train_classes)
 
 # model.save('model.tflearn')
 # model.load('./model.tflearn')
 
 
 # Evaluate model
-score = model.evaluate()
-print(score)
+# score = model.evaluate(test_features, train_classes)
+# print(score)
+#
+# predictions = model.predict(test_features)
+# predictions = np.array([np.argmax(pred) for pred in predictions])
+# train_classes = np.array([np.argmax(each) for each in train_classes])
+#
+# acc = 100 * np.sum(predictions == train_classes) / len(train_classes)
+#
+# print(acc)
 
 # Run the model on one example
-prediction = model.predict()
-print(prediction)
+# prediction = model.predict()
+# print(prediction)
 
 
 def input_sentence(sentence):

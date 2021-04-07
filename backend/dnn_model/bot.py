6f965e64353726e571ff17fd86b930e215d40195
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-04-07 20:08:10 +0530

    User input classification to respective classes

--- backend/dnn_model/bot.py
+++ backend/dnn_model/bot.py
@@ -1,13 +1,12 @@
-import nltk
-from nltk import LancasterStemmer
+from random import random
 
-import numpy as np
+from nltk import LancasterStemmer, word_tokenize
 
 from memory_growth import memory_growth_exception
-from preprocess import punctuation_removal
 from model import create_model
-from input_data import train_features, train_classes, test_features, features, words
-from user_input import input_sentence_words, bag_of_words_user_query
+from input_data import train_features, train_classes, test_features, features, words, intents, classes
+from preprocess import punctuation_removal
+from numpy import array
 
 stemmer = LancasterStemmer()
 
@@ -15,34 +14,54 @@ memory_growth_exception()
 
 model = create_model(features, train_classes)
 
-# model.save('model.tflearn')
-# model.load('./model.tflearn')
 
-temp_train_classes = []
-
-for t in train_classes:
-    temp_train_vector = np.pad(t, (0, 52), 'constant')
-    temp_train_classes.append(temp_train_vector)
+def input_sentence_words(sentence):
+    sentence = punctuation_removal(sentence)
+    sentence_words = word_tokenize(sentence)
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
 
 
-# Evaluate model
-score = model.evaluate(test_features, temp_train_classes)
-print(score)
-#
-# predictions = model.predict(test_features)
-# predictions = np.array([np.argmax(pred) for pred in predictions])
-# train_classes = np.array([np.argmax(each) for each in train_classes])
-#
-# acc = 100 * np.sum(predictions == train_classes) / len(train_classes)
-#
-# print(acc)
+def bag_of_words(sentence, word_set):
+    sentence_words = input_sentence_words(sentence)
+    bag = [0] * len(word_set)
+    for s_words in sentence_words:
+        for i, words in enumerate(word_set):
+            if words == s_words:
+                bag[i] = 1
+    return array(bag)
 
-# Run the model on one example
-prediction = model.predict()
-print(prediction)
 
-# user_sentence = input_sentence_words(input())
+error_threshold = 0.30
 
-# bog_array = bag_of_words_user_query(user_sentence, words)
 
-# print(bog_array)
+def classify(sentence):
+    # generate probabilities from the model
+    results = model.predict([bag_of_words(sentence, words)])[0]
+    # filter out predictions below a threshold
+    results = [[i, r] for i, r in enumerate(results) if r > error_threshold]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((train_classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    print(return_list)
+    return return_list
+
+
+def response(sentence):
+    results = classify(sentence)
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # a random response from the intent
+                    return print(random.choice(i['responses']))
+            results.pop(0)
+
+
+
+classify('what are my sick leaves?')

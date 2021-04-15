5f51fb373fa94d35fc1b9c83822028863047944c
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-04-15 20:24:47 +0530

    Testing for different vector representations

--- backend/dnn_model/input_data.py
+++ backend/dnn_model/input_data.py
@@ -5,12 +5,17 @@ from numpy import array
 
 from file_reader import read_intents_file
 from preprocess import punctuation_removal
+from sklearn.feature_extraction.text import TfidfVectorizer
+from word_to_vec import word2vec_skipgrams
+
+TfidfVectorizerObject = TfidfVectorizer()
 
 stemmer = LancasterStemmer()
 
 intents = read_intents_file()
 
 words = []
+sentences = []
 classes = []
 documents = []
 
@@ -19,6 +24,7 @@ for intent in intents['intents']:
         # pattern = pattern.lower()
         pattern = punctuation_removal(pattern)
         # pattern = get_lemmatized_words(pattern)
+        sentences.append(pattern)
         w = nltk.word_tokenize(pattern)
         words.extend(w)
 
@@ -33,15 +39,17 @@ words = sorted(list(set(words)))
 # remove duplicate classes
 classes = sorted(list(set(classes)))
 
-print(len(documents), "documents", documents)
-print(len(classes), "classes", classes)
-print(len(words), " words", words)
+# --------------------------- TRAINING TESTING VECTOR SECTION START ---------------------
+
+# print(len(documents), "documents", documents)
+# print(len(classes), "classes", classes)
+# print(len(words), " words", words)
 
 training = []
 output = []
 # create an empty array for output
 output_empty = [0] * len(classes)
-print(output_empty)
+# print(output_empty)
 
 for doc in documents:
     # initialize bag_of_words of words
@@ -59,23 +67,58 @@ for doc in documents:
 
     training.append([bag_of_words, output_row])
 
-for h in training:
-    print(h)
+# for h in training:
+#     print(h)
 
 random.shuffle(training)
 training = array(training, dtype=object)
 
 # creating training lists
-features = list(training[:, 0])  # features
-train_classes = list(training[:, 1])  # classes
+features = list(training[:, 0])  # feature vectors
+bog_train_classes = list(training[:, 1])  # classes
 
-print(len(features))
+# print(len(features))
 
-train_features = features[:(len(features) - 5)]
-print(len(train_features))
-test_features = features[-5:]
-print(len(test_features))
+bog_train_features = features[:(len(features) - 5)]
+bog_test_features = features[-5:]
+# print(len(train_features))
+# print(len(test_features))
+#
+# print("\n\n")
+# print(train_features)
+# print(train_classes)
+#
+# print(sentences)
 
-print("\n\n")
-print(train_features)
-print(train_classes)
+# --------------------------- TRAINING TESTING VECTOR SECTION END ---------------------
+
+# ------------------- Word2Vec Section -----------------------
+
+word2vec_train_features = word2vec_skipgrams(words)
+word2vec_class_vectors = word2vec_skipgrams(classes)
+
+
+# print(feature_vectors)
+# print(class_vectors)
+#
+# print(train_features)
+# print(train_classes)
+
+
+# ------------------- TFIDF SECTION --------------------------
+# feature_vectors = TfidfVectorizerObject.fit_transform(words)
+# print(feature_vectors)
+# fArr = feature_vectors.toarray()
+# for f in fArr:
+#     print(f)
+# class_vectors = TfidfVectorizerObject.fit_transform(classes)
+# print(class_vectors)
+# cArr = class_vectors.toarray()
+# for c in cArr:
+#     print(c)
+# print(bog_train_features)
+# print(word2vec_train_features)
+
+print(word2vec_train_features)
+print(word2vec_class_vectors)
+

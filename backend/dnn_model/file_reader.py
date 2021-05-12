9ba8bcf19dd2c9baf9b9697df0de75958d55d7a3
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-05-12 23:04:17 +0530

    removed prints

--- backend/dnn_model/file_reader.py
+++ backend/dnn_model/file_reader.py
@@ -5,7 +5,4 @@ def read_intents_file():
     filepath = 'corpus/intents.json'
     with open(filepath) as json_data:
         intents = json.load(json_data)
-    print(intents)
     return intents
-
-read_intents_file()

c116d26f054de36445f73ec3273ed97d542d58b6
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-05-12 23:03:51 +0530

    .

--- backend/dnn_model/dnn_model.py
+++ backend/dnn_model/dnn_model.py
@@ -6,11 +6,11 @@ def create_model(x_train, y_train):
     ops.reset_default_graph()
     network_layers = tflearn.input_data(shape=[None, len(x_train[0])])
     # network_layers = tflearn.embedding(incoming=network_layers, input_dim=10, output_dim=4)
+    # network_layers = tflearn.lstm(network_layers, 10)
     network_layers = tflearn.fully_connected(network_layers, 10)
     network_layers = tflearn.fully_connected(network_layers, 10)
     network_layers = tflearn.fully_connected(network_layers, len(y_train[0]), activation='softmax')
-    network_layers = tflearn.regression(network_layers, optimizer='adam', learning_rate=0.01)
+    network_layers = tflearn.regression(network_layers, optimizer='adam')
     model = tflearn.DNN(network_layers, tensorboard_dir='tflearn_logs')
-    model = tflearn.DNN(network_layers, tensorboard_verbose='3')
     model.fit(x_train, y_train, n_epoch=1000, batch_size=8, show_metric=True)
     return model

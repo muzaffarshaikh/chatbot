2f894b66b00a6817ac5969d24b85ba4fb63390a4
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-06-01 00:12:15 +0530

    Can handle training logs now

--- backend/dnn_model/dnn_model.py
+++ backend/dnn_model/dnn_model.py
@@ -2,13 +2,17 @@ import tflearn
 from tensorflow.python.framework import ops
 
 
-def create_model(x_train, y_train):
+def create_model(x_train, y_train, train):
     ops.reset_default_graph()
     network_layers = tflearn.input_data(shape=[None, len(x_train[0])])
     network_layers = tflearn.fully_connected(network_layers, 10)
     network_layers = tflearn.fully_connected(network_layers, 10)
     network_layers = tflearn.fully_connected(network_layers, len(y_train[0]), activation='softmax')
     network_layers = tflearn.regression(network_layers, optimizer='adam')
-    model = tflearn.DNN(network_layers, tensorboard_dir='tflearn_logs')
-    model.fit(x_train, y_train, n_epoch=1000, batch_size=8, show_metric=True)
+    if train is True:
+        model = tflearn.DNN(network_layers, tensorboard_dir='tflearn_logs')
+        model.fit(x_train, y_train, n_epoch=1000, batch_size=8, show_metric=True)
+    else:
+        model = tflearn.DNN(network_layers)
+        model.load('./model.tflearn')
     return model

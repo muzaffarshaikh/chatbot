be6569e8aff562398f47b0463b2fa0b5908a28d8
Muzaffar Shaikh <mzfrxec@gmail.com>
2021-06-02 23:10:57 +0530

    Added function to save chat messages

--- backend/dnn_model/user_profile.py
+++ backend/dnn_model/user_profile.py
@@ -1,8 +1,15 @@
+import json
+
 from flask import Flask, session
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import datetime
+import time
+import pymysql
+
+connection = pymysql.connect(host="localhost", user="root", passwd="", database="chatbot")
 
 app = Flask(__name__)
 CORS(app)
@@ -24,19 +31,19 @@ def register_db(email, name, password):
 
     if account:
         msg = 'Account already exists !'
-        return msg
+        return False, msg
     elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
         msg = 'Invalid email address !'
-        return msg
+        return False, msg
     elif not re.match(r'[A-Za-z0-9]+', email):
         msg = 'name must contain only characters and numbers !'
-        return msg
+        return False, msg
     else:
         cursor.execute('INSERT INTO users VALUES (NULL, % s, % s, % s)',
                        (email, name, password))
         mysql.connection.commit()
         msg = 'You have successfully registered !'
-        return msg
+        return True, msg
 
 
 def login_db(email, password):
@@ -59,3 +66,23 @@ def logout():
     # RETURNING.
     # By default itll be FALSE since user will be logged in
     # return TRUE when function is invoked and route to login page
+
+
+def save_chat(email, client_request, server_response):
+    cursor = connection.cursor()
+
+    emailQuery = 'select id from users where email="' + email + '"'
+    cursor.execute(emailQuery)
+    userIDTuple = cursor.fetchone()
+    userID = userIDTuple[0]
+
+    formatted_date = str(datetime.date.today())
+    current_date = formatted_date
+
+    formatted_time = time.strftime("%H:%M:%S", time.localtime())
+    current_time = formatted_time
+
+    chatJSONObject = str(json.dumps({'user_message': client_request, 'bot_response': server_response}))
+
+    cursor.execute('INSERT INTO chats VALUES (NULL, % s, % s, % s, % s)', (userID, chatJSONObject, current_date, current_time))
+    connection.commit()
